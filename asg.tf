84b4324ec2117694029442661d5e13498fc942c6
Mukesh <msahu201989@gmail.com>
2022-12-10 22:07:05 +0530

    Rabbit MQ

---
+++ asg.tf
@@ -0,0 +1,32 @@
+resource "aws_launch_template" "launch-template" {
+  name          = "${var.env}-${var.name}-lt"
+  image_id      = data.aws_ami.centos-8-ami.image_id
+  instance_type = var.instance_type
+}
+
+resource "aws_autoscaling_group" "asg" {
+  desired_capacity       = var.min_size
+  max_size               = var.max_size
+  min_size               = var.min_size
+  vpc_zone_identifier    = var.subnets
+
+  launch_template {
+    id                = aws_launch_template.launch-template.id
+    version = "$Latest"
+  }
+}
+
+#  iam_instance_profile {
+#    name = aws_iam_instance_profile.instance_profile.name
+#  }
+#
+#  instance_market_options {
+#    market_type = "spot"
+#  }
+#
+#
+#  user_data = base64encode(templatefile("${path.module}/ansible-pull.sh", {
+#    COMPONENT = var.name
+#    ENV       = var.env
+#  }))
+#}

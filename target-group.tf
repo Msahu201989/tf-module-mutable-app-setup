f4565a020cd3cd084ec51ddbde5ea3b88ae654ca
Mukesh <msahu201989@gmail.com>
2022-12-13 22:08:34 +0530

    listners

--- target-group.tf
+++ target-group.tf
@@ -27,7 +27,7 @@ resource "aws_lb_listener_rule" "rule" {
 
   condition {
     host_header {
-      values = ["${var.env}.mukeshsahu20.online"]
+      values = ["${var.env}.roboshop.internal"]
     }
   }
 
